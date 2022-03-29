690b3a33b4dac4c3148a1d89df648fe7257e836e
taufanbasri <taufanprasetyobasri@gmail.com>
2022-03-29 18:34:56 +0700

    Coded by

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: [],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nft': {
+          soft: "#2b73da",
+          main: "#0D192B",
+          card: "#14253D",
+          line: "#2F415B",
+          cyan: "#00FFF7"
+        }
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
